667f02c522da6c8708e1dd19c315b06239c5081f
Matthew Brooks <mb964033@gmail.com>
2024-01-19 19:31:52 +0000

    correct downlaod links

--- make.lua
+++ make.lua
@@ -1,24 +1,30 @@
-return {
-  type = "lib",
-  env = {
+local env = {
 
-    name = "tbhss",
-    version = "0.0.1-1",
-    variable_prefix = "TBHSS",
-    public = true,
+  name = "tbhss",
+  version = "0.0.2-1",
+  variable_prefix = "TBHSS",
+  public = true,
 
+  dependencies = {
+    "lua == 5.1"
+  },
+
+  test = {
     dependencies = {
-      "lua == 5.1"
-    },
-
-    test = {
-      dependencies = {
-        "santoku-test == 0.0.7-1",
-        "santoku-fs == 0.0.13-1",
-        "inspect == 3.1.3-0",
-        "luacov == 0.15.0-1",
-      }
-    },
+      "santoku-test == 0.0.7-1",
+      "santoku-fs == 0.0.13-1",
+      "inspect == 3.1.3-0",
+      "luacov == 0.15.0-1",
+    }
+  },
 
   }
+
+env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
+env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
+
+return {
+  type = "lib",
+  env = env,
 }
