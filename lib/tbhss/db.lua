bd9a3248fd0a6ccf063c33bdf493e8ca13d32d74
Matthew Brooks <mb964033@gmail.com>
2024-01-24 03:06:34 +0000

    fix sleeper bug

--- lib/tbhss/db.lua
+++ lib/tbhss/db.lua
@@ -27,79 +27,79 @@ return function (db_file)
 
     M.add_model = check(db:inserter([[
       insert into models (tag, dimensions)
-      values ($1, $2)
+      values (?, ?)
     ]]))
 
     M.add_clustering = check(db:inserter([[
       insert into clusterings (id_model, clusters)
-      values ($1, $2)
+      values (?, ?)
     ]]))
 
     M.set_words_loaded = check(db:runner([[
       update models
       set words_loaded = true
-      where id = $1
+      where id = ?
     ]]))
 
     M.set_words_clustered = check(db:runner([[
       update clusterings
       set words_clustered = true,
-          iterations = $2
-      where id = $1
+          iterations = ?2
+      where id = ?1
     ]]))
 
     M.get_model_by_tag = check(db:getter([[
       select *
       from models
-      where tag = $1
+      where tag = ?
     ]]))
 
     M.get_model_by_id = check(db:getter([[
       select *
       from models
-      where id = $1
+      where id = ?
     ]]))
 
     M.get_clustering = check(db:getter([[
       select *
       from clusterings
-      where id_model = $1
-      and clusters = $2
+      where id_model = ?
+      and clusters = ?
     ]]))
 
     M.add_word = check(db:inserter([[
       insert into words (id_model, name, id, vector)
-      values ($1, $2, $3, $4)
+      values (?, ?, ?, ?)
     ]]))
 
     M.set_word_cluster_similarity = check(db:inserter([[
       insert into clusters (id_clustering, id_word, id_cluster, similarity)
-      values ($1, $2, $3, $4)
+      values (?, ?, ?, ?)
     ]]))
 
     M.get_words = check(db:iter([[
       select id, name, vector from words
-      where id_model = $1
+      where id_model = ?
       order by id asc
     ]]))
 
     M.get_total_words = check(db:getter([[
       select count(*) as n
       from words
-      where id_model = $1
+      where id_model = ?
     ]], "n"))
 
     M.get_word_clusters = check(db:iter([[
       select w.name, w.id, c.id_cluster, c.similarity
       from models m, words w, clusters c
-      where m.tag = $1
+      where m.tag = ?
       and w.id_model = m.id
       and c.id_word = w.id
     ]]))
 
     M.delete_model_by_tag = check(db:runner([[
       delete from models
-      where tag = $1
+      where tag = ?
     ]]))
 
     return M
