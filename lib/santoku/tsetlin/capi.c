c09a4a226cb24750d40ef43f152f006b8b679c14
Matthew Brooks <mb964033@gmail.com>
2024-02-23 02:08:35 +0000

    wip

--- lib/santoku/tsetlin/capi.c
+++ lib/santoku/tsetlin/capi.c
@@ -9,26 +9,19 @@
 typedef struct {
 
   lua_Integer features;
-  lua_Integer classes;
   lua_Integer clauses;
   lua_Integer states;
   lua_Number threshold;
   bool boost_true_positive;
 
-  lua_Integer *automata_states; // [features][classes][clauses][polarity]
-  lua_Integer *clause_outputs; // [classes][clauses]
-  lua_Integer *clause_feedback; // [classes][clauses]
+  lua_Integer *automata_states; // [features][clauses][polarity]
+  lua_Integer *clause_outputs; // [clauses]
+  lua_Integer *clause_feedback; // [clauses]
 
 } tk_tsetlin_t;
 
-// TODO: Refine this ordering to maximize data locality at runtime
-#define tk_tsetlin_automata_idx(t, f, c, l, p) \
-  (f + (c * (t)->features) \
-     + (l * (t)->features * (t)->classes) \
-     + (p * (t)->features * (t)->classes * (t)->clauses))
-#define tk_tsetlin_clause_idx(t, c, l) \
-  (c + (l * (t)->clauses))
-
+#define tk_tsetlin_automata_idx(t, f, l, p) (f + (l * (t)->features * (p ? 2 : 1)))
+#define tk_tsetlin_clause_idx(t, l) (l)
 #define tk_tsetlin_action(t, n) (n > (t)->states)
 
 tk_tsetlin_t *tk_tsetlin_peek (lua_State *L, int i)
@@ -50,20 +43,19 @@ int tk_tsetlin_create (lua_State *L)
   if (!tm0)
     goto err_mem;
 
-  lua_settop(L, 6);
+  lua_settop(L, 5);
 
   tm0->features = luaL_checkinteger(L, 1);
-  tm0->classes = luaL_checkinteger(L, 2);
-  tm0->clauses = luaL_checkinteger(L, 3);
-  tm0->states = luaL_checkinteger(L, 4);
-  tm0->threshold = luaL_checknumber(L, 5);
+  tm0->clauses = luaL_checkinteger(L, 2);
+  tm0->states = luaL_checkinteger(L, 3);
+  tm0->threshold = luaL_checknumber(L, 4);
 
-  luaL_checktype(L, 6, LUA_TBOOLEAN);
-  tm0->boost_true_positive = lua_toboolean(L, 6);
+  luaL_checktype(L, 5, LUA_TBOOLEAN);
+  tm0->boost_true_positive = lua_toboolean(L, 5);
 
-  tm0->automata_states = malloc(sizeof(*tm0->automata_states) * tm0->features * tm0->classes * tm0->clauses * 2);
-  tm0->clause_outputs = malloc(sizeof(*tm0->clause_outputs) * tm0->classes * tm0->clauses);
-  tm0->clause_feedback = malloc(sizeof(*tm0->clause_outputs) * tm0->classes * tm0->clauses);
+  tm0->automata_states = malloc(sizeof(*tm0->automata_states) * tm0->features * tm0->clauses * 2);
+  tm0->clause_outputs = malloc(sizeof(*tm0->clause_outputs) * tm0->clauses);
+  tm0->clause_feedback = malloc(sizeof(*tm0->clause_feedback) * tm0->clauses);
 
   if (!(tm0->automata_states || tm0->clause_outputs || tm0->clause_feedback))
     goto err_mem;
@@ -74,15 +66,13 @@ int tk_tsetlin_create (lua_State *L)
   //   [0, tm0->states] and
   //   [tm0->states, tm0->states * 2]
   for (lua_Integer f = 0; f < tm0->features; f ++) {
-    for (lua_Integer c = 0; c < tm0->classes; c ++) {
-      for (lua_Integer l = 0; l < tm0->clauses; l ++) {
-        if (1.0 * rand() / RAND_MAX <= 0.5) {
-          tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, c, l, 0)] = tm0->states;
-          tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, c, l, 1)] = tm0->states + 1;
-        } else {
-          tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, c, l, 0)] = tm0->states + 1;
-          tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, c, l, 1)] = tm0->states;
-        }
+    for (lua_Integer l = 0; l < tm0->clauses; l ++) {
+      if (1.0 * rand() / RAND_MAX <= 0.5) {
+        tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, l, 0)] = tm0->states;
+        tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, l, 1)] = tm0->states + 1;
+      } else {
+        tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, l, 0)] = tm0->states + 1;
+        tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, l, 1)] = tm0->states;
       }
     }
   }
@@ -113,23 +103,23 @@ void tk_tsetlin_callmod (lua_State *L, int nargs, int nret, const char *smod, co
   lua_call(L, nargs, nret); // results
 }
 
-void _tk_tsetlin_calculate_clause_output (lua_State *L, tk_tsetlin_t *tm0, lua_Integer c, bool predict)
+void _tk_tsetlin_calculate_clause_output (lua_State *L, tk_tsetlin_t *tm0, bool predict)
 {
 	lua_Integer action_include, action_include_negated;
 	lua_Integer all_exclude;
 	for (lua_Integer l = 0; l < tm0->clauses; l ++) {
-    lua_Integer clause_idx = tk_tsetlin_clause_idx(tm0, c, l);
+    lua_Integer clause_idx = tk_tsetlin_clause_idx(tm0, l);
     tm0->clause_outputs[clause_idx] = 1;
 		all_exclude = 1;
 		for (lua_Integer f = 0; f < tm0->features; f ++) {
-			action_include = tk_tsetlin_action(tm0, tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, c, l, 0)]);
-			action_include_negated = tk_tsetlin_action(tm0, tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, c, l, 1)]);
+			action_include = tk_tsetlin_action(tm0, tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, l, 0)]);
+			action_include_negated = tk_tsetlin_action(tm0, tm0->automata_states[tk_tsetlin_automata_idx(tm0, f, l, 1)]);
 			all_exclude = all_exclude && !(action_include == 1 || action_include_negated == 1);
-      lua_pushvalue(L, -2); // problem solution problem
-      lua_pushinteger(L, f + 1); // problem solution problem idx
-      tk_tsetlin_callmod(L, 2, 1, "santoku.bitmap", "get");
+      lua_pushvalue(L, -1); // problem problem
+      lua_pushinteger(L, f + 1); // problem problem idx
+      tk_tsetlin_callmod(L, 2, 1, "santoku.bitmap", "get"); // problem bool
       bool is_set = lua_toboolean(L, -1);
-      lua_pop(L, 1); // problem solution
+      lua_pop(L, 1); // problem
 			if ((action_include == 1 && !is_set) || (action_include_negated == 1 && is_set)) {
         tm0->clause_outputs[clause_idx] = 0;
 				break;
@@ -139,26 +129,26 @@ void _tk_tsetlin_calculate_clause_output (lua_State *L, tk_tsetlin_t *tm0, lua_I
 	}
 }
 
-lua_Integer _tk_tsetlin_sum_class_votes (lua_State *L, tk_tsetlin_t *tm0, lua_Integer c)
+lua_Integer _tk_tsetlin_sum_class_votes (lua_State *L, tk_tsetlin_t *tm0)
 {
   lua_Integer class_sum = 0;
   for (lua_Integer l = 0; l < tm0->clauses; l ++) {
     int sign = 1 - 2 * (l & 1);
-    class_sum += tm0->clause_outputs[tk_tsetlin_clause_idx(tm0, c, l)] * sign;
+    class_sum += tm0->clause_outputs[tk_tsetlin_clause_idx(tm0, l)] * sign;
   }
   class_sum = (class_sum > tm0->threshold) ? tm0->threshold : class_sum;
   class_sum = (class_sum < -tm0->threshold) ? -tm0->threshold : class_sum;
   return class_sum;
 }
 
-void _tk_tsetlin_type_ii_feedback (lua_State *L, tk_tsetlin_t *tm0, lua_Integer c, lua_Integer l)
+void _tk_tsetlin_type_ii_feedback (lua_State *L, tk_tsetlin_t *tm0, lua_Integer l)
 {
 	lua_Integer action_include;
 	lua_Integer action_include_negated;
-  if (tm0->clause_outputs[tk_tsetlin_clause_idx(tm0, c, l)]) {
+  if (tm0->clause_outputs[tk_tsetlin_clause_idx(tm0, l)]) {
 		for (lua_Integer f = 0; f < tm0->features; f ++) {
-      lua_Integer idx0 = tk_tsetlin_automata_idx(tm0, f, c, l, 0);
-      lua_Integer idx1 = tk_tsetlin_automata_idx(tm0, f, c, l, 1);
+      lua_Integer idx0 = tk_tsetlin_automata_idx(tm0, f, l, 0);
+      lua_Integer idx1 = tk_tsetlin_automata_idx(tm0, f, l, 1);
 			action_include = tk_tsetlin_action(tm0, tm0->automata_states[idx0]);
 			action_include_negated = tk_tsetlin_action(tm0, tm0->automata_states[idx1]);
       tm0->automata_states[idx0] += (action_include == 0 && tm0->automata_states[idx0] < tm0->states * 2);
@@ -167,25 +157,25 @@ void _tk_tsetlin_type_ii_feedback (lua_State *L, tk_tsetlin_t *tm0, lua_Integer 
 	}
 }
 
-void _tk_tsetlin_type_i_feedback (lua_State *L, tk_tsetlin_t *tm0, lua_Integer c, lua_Integer l, lua_Number s)
+void _tk_tsetlin_type_i_feedback (lua_State *L, tk_tsetlin_t *tm0, lua_Integer l, lua_Number s)
 {
-  lua_Integer clause_idx = tk_tsetlin_clause_idx(tm0, c, l);
+  lua_Integer clause_idx = tk_tsetlin_clause_idx(tm0, l);
 	if (tm0->clause_outputs[clause_idx] == 0)	{
 		for (int f = 0; f < tm0->features; f ++) {
-      lua_Integer idx0 = tk_tsetlin_automata_idx(tm0, c, l, f, 0);
-      lua_Integer idx1 = tk_tsetlin_automata_idx(tm0, c, l, f, 1);
+      lua_Integer idx0 = tk_tsetlin_automata_idx(tm0, l, f, 0);
+      lua_Integer idx1 = tk_tsetlin_automata_idx(tm0, l, f, 1);
       tm0->automata_states[idx0] -= tm0->automata_states[idx0] && (1.0 * rand() / RAND_MAX <= 1.0 / s);
       tm0->automata_states[idx1] -= tm0->automata_states[idx1] && (1.0 * rand() / RAND_MAX <= 1.0 / s);
 		}
 	} else if (tm0->clause_outputs[clause_idx] == 1) {
 		for (int f = 0; f < tm0->features; f ++) {
-      lua_pushvalue(L, -2); // problem solution problem
-      lua_pushinteger(L, f + 1); // problem solution problem idx
-      tk_tsetlin_callmod(L, 2, 1, "santoku.bitmap", "get");
+      lua_pushvalue(L, -1); // problem problem
+      lua_pushinteger(L, f + 1); // problem problem idx
+      tk_tsetlin_callmod(L, 2, 1, "santoku.bitmap", "get"); // problem bool
       bool is_set = lua_toboolean(L, -1);
-      lua_pop(L, 1); // problem solution
-      lua_Integer idx0 = tk_tsetlin_automata_idx(tm0, c, l, f, 0);
-      lua_Integer idx1 = tk_tsetlin_automata_idx(tm0, c, l, f, 1);
+      lua_pop(L, 1); // problem
+      lua_Integer idx0 = tk_tsetlin_automata_idx(tm0, l, f, 0);
+      lua_Integer idx1 = tk_tsetlin_automata_idx(tm0, l, f, 1);
 			if (is_set) {
 				tm0->automata_states[idx0] += (tm0->automata_states[idx0] < tm0->states * 2)
           && (tm0->boost_true_positive == 1 || 1.0 * rand() / RAND_MAX <= (s - 1) / s);
@@ -201,12 +191,12 @@ void _tk_tsetlin_type_i_feedback (lua_State *L, tk_tsetlin_t *tm0, lua_Integer c
 	}
 }
 
-void _tk_tsetlin_update_class (lua_State *L, tk_tsetlin_t *tm0, lua_Integer c, lua_Integer tgt, lua_Number s)
+void _tk_tsetlin_update (lua_State *L, tk_tsetlin_t *tm0, lua_Integer tgt, lua_Number s)
 {
-	_tk_tsetlin_calculate_clause_output(L, tm0, c, false); // problem solution
-	lua_Integer class_sum = _tk_tsetlin_sum_class_votes(L, tm0, c); // problem solution
+	_tk_tsetlin_calculate_clause_output(L, tm0, false); // problem
+	lua_Integer class_sum = _tk_tsetlin_sum_class_votes(L, tm0); // problem
 	for (lua_Integer l = 0; l < tm0->clauses; l ++) {
-    tm0->clause_feedback[tk_tsetlin_clause_idx(tm0, c, l)] =
+    tm0->clause_feedback[tk_tsetlin_clause_idx(tm0, l)] =
 		  (2 * tgt - 1) *
       (1 - 2 * (l & 1)) *
       (1.0 * rand() / RAND_MAX <=
@@ -214,40 +204,39 @@ void _tk_tsetlin_update_class (lua_State *L, tk_tsetlin_t *tm0, lua_Integer c, l
         (tm0->threshold + (1 - 2 * tgt) * class_sum));
   }
 	for (int l = 0; l < tm0->clauses; l ++) {
-    lua_Integer fb = tm0->clause_feedback[tk_tsetlin_clause_idx(tm0, c, l)];
-		if (fb > 0)
-			_tk_tsetlin_type_i_feedback(L, tm0, c, l, s); // problem solution
-		else if (fb < 0)
-			_tk_tsetlin_type_ii_feedback(L, tm0, c, l); // problem solution
+    lua_Integer fb = tm0->clause_feedback[tk_tsetlin_clause_idx(tm0, l)];
+		if (fb > 0) {
+			_tk_tsetlin_type_i_feedback(L, tm0, l, s); // problem
+    } else if (fb < 0) {
+			_tk_tsetlin_type_ii_feedback(L, tm0, l); // problem
+    }
 	}
 }
 
-void _tk_tsetlin_update (lua_State *L, tk_tsetlin_t *tm0, lua_Number s)
+lua_Integer _tk_tsetlin_score (lua_State *L, tk_tsetlin_t *tm0)
 {
-  for (lua_Integer c = 0; c < tm0->classes; c ++) {
-    _tk_tsetlin_update_class(L, tm0, c, 1, s); // problem solution
-    lua_Integer c0;
-    for (c0 = c; c0 == c; c0 = tm0->classes * 1.0 * rand() / RAND_MAX + 1);
-    _tk_tsetlin_update_class(L, tm0, c0, 0, s); // problem solution
-  }
+	_tk_tsetlin_calculate_clause_output(L, tm0, false);
+  return _tk_tsetlin_sum_class_votes(L, tm0);
 }
 
-int tk_tsetlin_train (lua_State *L)
+int tk_tsetlin_predict (lua_State *L)
+{
+  lua_settop(L, 2);
+  tk_tsetlin_t *tm0 = tk_tsetlin_peek(L, 1);
+  luaL_checkudata(L, 2, "santoku_bitmap");
+  lua_pushboolean(L, tk_tsetlin_action(tm0, _tk_tsetlin_score(L, tm0)));
+  return 1;
+}
+
+int tk_tsetlin_update (lua_State *L)
 {
   lua_settop(L, 4);
   tk_tsetlin_t *tm0 = tk_tsetlin_peek(L, 1);
-  luaL_checktype(L, 2, LUA_TTABLE);
-  luaL_checktype(L, 3, LUA_TTABLE);
+  luaL_checkudata(L, 2, "santoku_bitmap");
+  lua_Integer tgt = luaL_checknumber(L, 3);
   lua_Number s = luaL_checknumber(L, 4);
-  lua_Integer nprob = lua_objlen(L, 2);
-  for (lua_Integer i = 1; i <= nprob; i++) {
-    lua_pushinteger(L, i);
-    lua_gettable(L, 2); // problem
-    lua_pushinteger(L, i);
-    lua_gettable(L, 3); // problem solution
-    _tk_tsetlin_update(L, tm0, s);
-    lua_pop(L, 2);
-  }
+  lua_pushvalue(L, 2);
+  _tk_tsetlin_update(L, tm0, tgt, s);
   return 0;
 }
 
@@ -255,7 +244,8 @@ luaL_Reg tk_tsetlin_fns[] =
 {
   { "create", tk_tsetlin_create },
   { "destroy", tk_tsetlin_destroy },
-  { "train", tk_tsetlin_train },
+  { "update", tk_tsetlin_update },
+  { "predict", tk_tsetlin_predict },
   { NULL, NULL }
 };
 
