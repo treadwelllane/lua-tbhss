0b0682e287b350778951c4ef3aeb444563505cc4
Matthew Brooks <mb964033@gmail.com>
2024-02-26 19:31:28 +0000

    cleanup

---
+++ test/spec/tbhss/bitmaps.lua
@@ -0,0 +1,30 @@
+local rand = require("santoku.random")
+local fs = require("santoku.fs")
+local env = require("santoku.env")
+local init_db = require("tbhss.db")
+local glove = require("tbhss.glove")
+local cluster = require("tbhss.cluster")
+local bitmaps = require("tbhss.bitmaps")
+
+rand.seed()
+
+local glove_file = env.var("GLOVE_TXT", "test/res/glove.2500.txt")
+local db_file = "tmp/test.db"
+
+local clusters = 16
+local bitmap_scale_factor = 8
+
+fs.mkdirp(fs.dirname(db_file))
+fs.rm(db_file, true)
+fs.rm(db_file .. "-wal", true)
+fs.rm(db_file .. "-shm", true)
+
+local db = init_db(db_file)
+local model, word_matrix--[[, _, word_names]] = glove.load_vectors(db, nil, glove_file, nil)
+local distance_matrix = cluster.cluster_vectors(db, model, word_matrix, clusters)
+--[[local word_bitmaps = ]]bitmaps.create_bitmaps(distance_matrix, bitmap_scale_factor)
+
+-- local it = require("santoku.iter")
+-- it.each(print, it.map(function (k, v)
+--   return k, string.format("%-15s", v), word_bitmaps[k]
+-- end, it.ipairs(word_names)))
