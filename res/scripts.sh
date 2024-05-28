96a2bb3f4f075ce1f9abc7b69c8723904a268671
Matthew Brooks <mb964033@gmail.com>
2024-05-28 17:32:19 +0000

    update tests

--- res/scripts.sh
+++ res/scripts.sh
@@ -15,69 +15,34 @@ nohup stdbuf -oL tbhss load sentences \
 
 nohup stdbuf -oL tbhss create clusters \
   --cache tbhss.db \
-  --name glove.6B.300d.128.train \
+  --name glove.6B.300d.128.test \
   --clusters 128 \
-  --filter-words snli_1.0.train \
-  --words glove.6B.300d 2>&1 > log.txt & tail -f log.txt
-
-nohup stdbuf -oL tbhss create clusters \
-  --cache tbhss.db \
-  --name glove.6B.300d.256.test \
-  --clusters 256 \
   --filter-words snli_1.0.test \
   --words glove.6B.300d 2>&1 > log.txt & tail -f log.txt
 
 nohup stdbuf -oL tbhss create bitmaps clustered \
   --cache tbhss.db \
-  --name glove.6B.300d.128.train.3.3.00 \
-  --clusters glove.6B.300d.128.train \
-  --min-set 3 \
-  --max-set 3 \
-  --min-similarity 0.00 2>&1 > log.txt & tail -f log.txt
-
-nohup stdbuf -oL tbhss create bitmaps clustered \
-  --cache tbhss.db \
-  --name glove.6B.300d.256.test.1.8.00 \
-  --clusters glove.6B.300d.256.test \
+  --name glove.6B.300d.128.test.1.8.00 \
+  --clusters glove.6B.300d.128.test \
   --min-set 1 \
   --max-set 8 \
-  --min-similarity 0.00 2>&1 > log.txt & tail -f log.txt
+  --min-similarity 0.0 2>&1 > log.txt & tail -f log.txt
 
 nohup stdbuf -oL tbhss create encoder \
   --cache tbhss.db \
-  --name glove \
-  --bitmaps glove.6B.300d.128.train.3.3.00  \
-  --sentences snli_1.0.train \
-  --encoded-bits 128 \
-  --train-test-ratio 0.5 \
-  --clauses 80 \
-  --state-bits 8 \
-  --threshold 256 \
-  --margin 0.1 \
-  --loss-alpha 1 \
-  --spec-min 2 \
-  --spec-max 40 \
-  --spec-alpha 1 \
-  --active-clause 0.85 \
-  --boost-true-positive false \
-  --evaluate-every 5 \
-  --epochs 100 2>&1 > log.txt & tail -f log.txt
-
-nohup stdbuf -oL tbhss create encoder \
-  --cache tbhss.db \
-  --name glove \
-  --bitmaps glove.6B.300d.256.test.1.8.00  \
+  --name glove.6B.300d.128.test.1.8.00.4.256 \
+  --bitmaps glove.6B.300d.64.test.1.4.00  \
   --sentences snli_1.0.test \
-  --segments 2 \
+  --segments 1 \
   --encoded-bits 256 \
   --train-test-ratio 0.5 \
-  --clauses 512 \
+  --clauses 1024 \
   --state-bits 8 \
   --threshold 256 \
   --margin 0.05 \
-  --loss-alpha 0.125 \
+  --loss-alpha 0.5 \
   --specificity 10 \
   --active-clause 0.85 \
   --boost-true-positive false \
   --evaluate-every 1 \
-  --epochs 100 2>&1 > log.txt & tail -f log.txt
+  --epochs 1000 2>&1 > log.txt & tail -f log.txt
