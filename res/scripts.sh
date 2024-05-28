68f41ab556bd46eddf745b0ecbec856e4f800247
Matthew Brooks <mb964033@gmail.com>
2024-05-28 14:04:15 +0000

    dynamic specificity

--- res/scripts.sh
+++ res/scripts.sh
@@ -20,12 +20,27 @@ nohup stdbuf -oL tbhss create clusters \
   --filter-words snli_1.0.train \
   --words glove.6B.300d 2>&1 > log.txt & tail -f log.txt
 
+nohup stdbuf -oL tbhss create clusters \
+  --cache tbhss.db \
+  --name glove.6B.300d.256.test \
+  --clusters 256 \
+  --filter-words snli_1.0.test \
+  --words glove.6B.300d 2>&1 > log.txt & tail -f log.txt
+
 nohup stdbuf -oL tbhss create bitmaps clustered \
   --cache tbhss.db \
   --name glove.6B.300d.128.train.3.3.00 \
   --clusters glove.6B.300d.128.train \
   --min-set 3 \
   --max-set 3 \
+  --min-similarity 0.00 2>&1 > log.txt & tail -f log.txt
+
+nohup stdbuf -oL tbhss create bitmaps clustered \
+  --cache tbhss.db \
+  --name glove.6B.300d.256.test.1.8.00 \
+  --clusters glove.6B.300d.256.test \
+  --min-set 1 \
+  --max-set 8 \
   --min-similarity 0.00 2>&1 > log.txt & tail -f log.txt
 
 nohup stdbuf -oL tbhss create encoder \
@@ -40,8 +55,30 @@ nohup stdbuf -oL tbhss create encoder \
   --threshold 256 \
   --margin 0.1 \
   --loss-alpha 1 \
-  --specificity 2 \
+  --spec-min 2 \
+  --spec-max 40 \
+  --spec-alpha 1 \
   --active-clause 0.85 \
   --boost-true-positive false \
   --evaluate-every 5 \
+  --epochs 100 2>&1 > log.txt & tail -f log.txt
+
+nohup stdbuf -oL tbhss create encoder \
+  --cache tbhss.db \
+  --name glove \
+  --bitmaps glove.6B.300d.256.test.1.8.00  \
+  --sentences snli_1.0.test \
+  --encoded-bits 256 \
+  --train-test-ratio 0.5 \
+  --clauses 1024 \
+  --state-bits 8 \
+  --threshold 256 \
+  --margin 0.05 \
+  --loss-alpha 2 \
+  --spec-min 2 \
+  --spec-max 40 \
+  --spec-alpha 1 \
+  --active-clause 0.85 \
+  --boost-true-positive false \
+  --evaluate-every 1 \
   --epochs 100 2>&1 > log.txt & tail -f log.txt
