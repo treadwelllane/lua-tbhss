a52c1c3d55f3f27699abdee3f03e1e5a234af854
Matthew Brooks <mb964033@gmail.com>
2025-04-09 02:49:09 +0000

    update

--- make.lua
+++ make.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "tbhss",
-  version = "0.0.53-1",
+  version = "0.0.54-1",
   variable_prefix = "TBHSS",
   public = true,
 
@@ -16,7 +16,7 @@ local env = {
     "santoku == 0.0.256-1",
     "santoku-fs == 0.0.33-1",
     "santoku-system == 0.0.46-1",
-    "santoku-tsetlin == 0.0.72-1",
+    "santoku-tsetlin == 0.0.73-1",
     "santoku-bitmap == 0.0.44-1",
     "santoku-matrix == 0.0.13-1",
     "santoku-sqlite == 0.0.22-1",
